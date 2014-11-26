7b7ebfff6c54d74e5c3c176b4fd383f9ff2280ec
Nicolas Richard <nicolasrichard3@gmail.com>
2014-11-25 16:42:46 -0800

    Create LRUCache.py

---
+++ LRUCache.py
@@ -0,0 +1,45 @@
+#My solution to the LRU Cache problem.
+#http://en.wikipedia.org/wiki/Cache_algorithms
+#https://oj.leetcode.com/problems/lru-cache/
+
+class LRUCache:
+
+    # @param capacity, an integer
+    def __init__(self, capacity):
+        self.capacity=capacity
+        self.capacity_used = 0
+        self.lru=[]
+        self.keymap={}
+        
+
+    # @return an integer
+    def get(self, key):
+        if self.keymap.has_key(key):
+            self.lru.remove(key)
+            self.lru.append(key)
+            return self.keymap[key]
+        else:
+            return -1
+
+    # @param key, an integer
+    # @param value, an integer
+    # @return nothing
+    def set(self, key, value):
+        if self.keymap.has_key(key) == False:
+            if self.capacity_used == self.capacity:
+                # insert new key by first making room for it
+                del self.keymap[self.lru[0]]
+                self.lru.remove(self.lru[0])
+                self.keymap[key]=value
+                self.lru.append(key)
+            else:
+                # insert a new key when there is still room
+                self.keymap[key]=value
+                self.lru.append(key)
+                self.capacity_used+=1    
+
+        else:
+            # I overide a key that I already have
+            self.keymap[key]=value
+            self.lru.remove(key)
+            self.lru.append(key)

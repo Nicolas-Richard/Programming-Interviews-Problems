3fd4ad4bd804e54513aca53761f0847c017057e2
Nicolas Richard <NicolasRichard3@gmail.com>
2014-11-26 16:48:09 -0800

    problems of 26/11

---
+++ The Algorithm Design Manual/binomial.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def binomialCoefficient(n,k):
+
+	# Let's build the Pascal triangle
+
+	triangle = np.zeros((n+1,n+1))
+	triangle[0][1]=1
+	for i in range(1,n+1):
+		for j in range(1,n+1):
+			triangle[i][j]=triangle[i-1][j-1] + triangle[i-1][j]
+	print triangle
+	return triangle[n][k+2]	
+
+print binomialCoefficient(5,2)
